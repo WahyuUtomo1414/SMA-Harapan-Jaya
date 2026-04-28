834be1ead8cd0db3560ea52c5502b7eae8089bbd
Wahyu Dwi Utomo <wahyuxd14@gmail.com>
2026-04-28 19:56:14 +0700

    ubah tahun

--- resources/views/sections/home/stats.blade.php
+++ resources/views/sections/home/stats.blade.php
@@ -3,7 +3,7 @@
     <div class="max-w-7xl mx-auto px-8">
         <div class="grid grid-cols-2 lg:grid-cols-4 gap-y-16">
             <div class="text-center group md:border-r border-gray-200 last:border-0 relative">
-                <h3 class="text-5xl md:text-7xl font-headline font-normal text-[#111] mb-6 italic">1985</h3>
+                <h3 class="text-5xl md:text-7xl font-headline font-normal text-[#111] mb-6 italic">1986</h3>
                 <p class="text-primary font-subhead text-[10px] uppercase tracking-[0.3em] font-bold">Tahun Berdiri</p>
             </div>
             <div class="text-center group md:border-r border-gray-200 last:border-0 relative">
