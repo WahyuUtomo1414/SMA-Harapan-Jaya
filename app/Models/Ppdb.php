1a0dc2236cb11ff57bf33f2226d07e22b13935d9
Wahyu Dwi Utomo <wahyuxd14@gmail.com>
2026-05-04 22:08:16 +0700

    update seeder ppdb

--- app/Models/Ppdb.php
+++ app/Models/Ppdb.php
@@ -11,7 +11,7 @@ class Ppdb extends Model
 {
     use HasFactory, AuditedBySoftDelete, SoftDeletes;
 
-    protected $table = 'form_ppdb';
+    protected $table = 'ppdb';
 
     protected $guarded = ['id'];
 
